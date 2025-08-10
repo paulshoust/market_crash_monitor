9cd8fbc8efe368f73f118866b498e81e2c3d62ea
paulshoust <paul.shoust@gmail.com>
2025-08-10 23:34:39 +0200

    docker fix-13

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "[entrypoint] TZ=${TZ:-Europe/Madrid} DATA_DIR=${DATA_DIR:-/data}"
 echo "[entrypoint] SCHEDULE_CRON=${SCHEDULE_CRON:-'0 17 * * 1'} RUN_AT_START=${RUN_AT_START:-0}"
 echo "[entrypoint] HEALTH_HTTP=${HEALTH_HTTP:-1} HEALTH_PORT=${HEALTH_PORT:-8087}"
 
-exec python /market_monitor/scheduler.py
+exec python scheduler.py
